09573e30716e5b46849c7173318637f0e5a0346f
JmadisonHGP <93689878+JmadisonHGP@users.noreply.github.com>
2022-07-18 15:52:47 -0700

    Updated Project
    Started levels 2 and 3

--- FirstProject/Assets/Scripts/AbilityPickup.cs
+++ FirstProject/Assets/Scripts/AbilityPickup.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AbilityPickUp : MonoBehaviour
+public class AbilityPickup : MonoBehaviour
 {
 
     public AbilityTypes abilityTypes;
